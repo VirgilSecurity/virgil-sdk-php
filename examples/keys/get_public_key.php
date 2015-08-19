a826c5565e4d47460e1b79f4cce1ad47b68284c4
DSuhinin <suhinin.dmitriy@gmail.com>
2015-08-19 12:01:35 +0300

    *working with examples *update Error codes for Keys and PrivateKeys services *documentation added

---
+++ examples/keys/get_public_key.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DSuhinin
+ * Date: 19.08.15
+ * Time: 10:37
+ */ 
--- src/SDK/PrivateKeys/Http/Error/Error.php
+++ src/SDK/PrivateKeys/Http/Error/Error.php
@@ -7,28 +7,38 @@ use Virgil\SDK\Common\Http\Error\Error as CommonError;
 class Error extends CommonError {
 
     protected static $_errorMap = array(
-        20001 => 'Authentication password validation failed',
-        20002 => 'Authentication user data validation failed',
-        20003 => 'Authentication account was not found by provided user data',
-        20004 => 'Authentication token validation failed',
-        20005 => 'Authentication token not found',
-        20006 => 'Authentication token has expired',
-        30001 => 'Signed validation failed',
-        40001 => 'Account validation failed',
-        40002 => 'Account was not found',
-        40003 => 'Account already exists',
-        40004 => 'Account password was not specified',
-        40005 => 'Account password validation failed',
-        40006 => 'Account was not found in PKI service',
-        40007 => 'Account type validation failed',
-        50001 => 'Public Key validation failed',
-        50002 => 'Public Key was not found',
-        50003 => 'Public Key already exists',
-        50004 => 'Public Key private key validation failed',
-        50005 => 'Public Key private key base64 validation failed',
-        60001 => 'Token was not found in request',
-        60002 => 'User Data validation failed',
-        60003 => 'Account was not found by user data',
-        60004 => 'Verification token has expired'
+        10001 => 'Internal application error. Route was not found.',
+        10002 => 'Internal application error. Route not allowed.',
+
+        20001 => 'Authentication error. Password validation failed',
+        20002 => 'Authentication error. User data validation failed',
+        20003 => 'Authentication error. Container was not found',
+        20004 => 'Authentication error. Token validation failed',
+        20005 => 'Authentication error. Token not found',
+        20006 => 'Authentication error. Token has expired',
+
+        30001 => 'Request Sign error. Request Sign validation failed',
+
+        40001 => 'Container error. Container validation failed',
+        40002 => 'Container error. Container was not found',
+        40003 => 'Container error. Container already exists',
+        40004 => 'Container error. Container password was not specified',
+        40005 => 'Container error. Container password validation failed',
+        40006 => 'Container error. Container was not found in PKI service',
+        40007 => 'Container error. Container type validation failed',
+
+        50001 => 'Key error. Public Key ID validation failed',
+        50002 => 'Key error. Public Key ID was not found',
+        50003 => 'Key error. Public Key ID already exists',
+        50004 => 'Key error. Private key validation failed',
+        50005 => 'Key error. Private key base64 validation failed',
+
+        60001 => 'Verification error. Token was not found in request',
+        60002 => 'Verification error. User Data validation failed',
+        60003 => 'Verification error. Container was not found',
+        60004 => 'Verification error. Verification token hash expired',
+
+        70001 => 'Application Token error. Application token invalid',
+        70002 => 'Application Token error. Application token service error'
     );
 }
