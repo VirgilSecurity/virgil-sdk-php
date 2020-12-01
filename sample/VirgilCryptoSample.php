1690260d661756ca6ca43516afd486013b145237
Yurii Kuzich <qzichs@gmail.com>
2020-12-01 23:25:33 +0200

    fix sample in sdk

--- sample/VirgilCryptoSample.php
+++ sample/VirgilCryptoSample.php
@@ -1,50 +1,43 @@
 <?php
 
+declare(strict_types=1);
+
 require '../vendor/autoload.php';
 
+use Virgil\Crypto\Core\VirgilKeys\VirgilKeyPair;
 use Virgil\Crypto\VirgilCrypto;
 use Virgil\Sdk\Card;
 use Virgil\Sdk\CardManager;
 use Virgil\Sdk\CardParams;
+use Virgil\Sdk\Http\VirgilAgent\HttpVirgilAgent;
 use Virgil\Sdk\Storage\PrivateKeyStorage;
 use Virgil\Sdk\Verification\VirgilCardVerifier;
 use Virgil\Sdk\Web\Authorization\CallbackJwtProvider;
 use Virgil\Sdk\Web\Authorization\JwtGenerator;
-use Virgil\Sdk\Web\Authorization\TokenContext;
+use \Virgil\Crypto\Core\VirgilKeys\VirgilPublicKeyCollection;
+use Virgil\Sdk\Web\CardClient;
+use \Virgil\Sdk\Storage\PrivateKeyEntry;
 
 /**
  * Class VirgilCryptoSample
  */
 class VirgilCryptoSample
 {
-    /**
-     * @var array
-     */
     private $auth;
 
-    /**
-     * @var
-     */
     private $identity;
 
-    /**
-     * @var \Virgil\CryptoImpl\VirgilKeyPair
-     */
     private $keyPair;
 
-    /**
-     * @var PrivateKeyStorage
-     */
     private $privateKeyStorage;
 
-    /**
-     * @var string
-     */
     private $storagePath = "./keys/";
 
     public function __construct()
     {
         $this->auth = [
+            'serviceAddress' => $_ENV["SERVICE_ADDRESS"],
+            'serviceKey' => $_ENV["SERVICE_KEY"],
             'apiKey' => $_ENV["SAMPLE_API_KEY"],
             'apiKeyId' => $_ENV["SAMPLE_API_KEY_ID"],
             'appId' => $_ENV["SAMPLE_APP_ID"],
@@ -53,188 +46,148 @@ class VirgilCryptoSample
 
         $this->keyPair = $this->generateKeys();
 
-        $exporter = new VirgilPrivateKeyExporter($_ENV["SAMPLE_PRIVATE_KEY_PASSWORD"]);
-        $this->privateKeyStorage = new PrivateKeyStorage($exporter, $this->storagePath);
+        $this->privateKeyStorage = new PrivateKeyStorage($this->getVirgilCrypto(), $this->storagePath);
     }
 
     // PUBLIC FUNCTIONS:
 
-    /**
-     * @param $identity
-     */
-    public function setIdentity($identity)
+    public function setIdentity(string $identity): void
     {
         $this->identity = $identity;
     }
 
-    /**
-     * @return Card
-     */
-    public function storePrivateKeyAndCreateCard()
+    public function storePrivateKeyAndCreateCard(): Card
     {
         $this->storePrivateKey($this->identity);
-        return $this->createCard($this->identity);
+        return $this->createCard();
     }
 
-    /**
-     * @param $recipientIdentity
-     * @param $dataToEncrypt
-     * @return mixed
-     */
-    public function signThenEncryptData($recipientIdentity, $dataToEncrypt)
+    public function signThenEncryptData(string $recipientIdentity, string $dataToEncrypt): ?string
     {
         $cards = $this->getUserCardsByIdentity($recipientIdentity);
 
-        $recipientCardsPublicKeys = array_map(
-            function (Card $card) {
-                return $card->getPublicKey();
-            },
-            $cards
-        );
+        $keyCollection = new VirgilPublicKeyCollection();
+        foreach ($cards as $card) {
+            $keyCollection->addPublicKey($card->getPublicKey());
+        }
 
-        $encryptedData = $this->getCrypto()->signThenEncrypt(
+        $encryptedData = $this->getVirgilCrypto()->signAndEncrypt(
             $dataToEncrypt,
             $this->loadPrivateKey($this->identity)->getPrivateKey(),
-            $recipientCardsPublicKeys
+            $keyCollection
         );
 
         return $encryptedData;
     }
 
-    /**
-     * @param $senderIdentity
-     * @param $dataToDecrypt
-     * @return mixed
-     */
-    public function decryptDataThenVerifySignature($senderIdentity, $dataToDecrypt)
+    public function decryptDataThenVerifySignature(string $senderIdentity, string $dataToDecrypt): ?string
     {
-        $cards = $this->getUserCardsByIdentity($senderIdentity);
+        $senderCards = $this->getUserCardsByIdentity($senderIdentity);
+        $keyCollection = new VirgilPublicKeyCollection();
 
-        $senderCardsPublicKeys = array_map(
-            function (Card $card) {
-                return $card->getPublicKey();
-            },
-            $cards
-        );
+        foreach ($senderCards as $card) {
+            $keyCollection->addPublicKey($card->getPublicKey());
+        }
 
-        $decryptedData = $this->getCrypto()->decryptThenVerify(
+        $decryptedData = $this->getVirgilCrypto()->decryptAndVerify(
             $dataToDecrypt,
             $this->loadPrivateKey($this->identity)->getPrivateKey(),
-            $senderCardsPublicKeys
+            $keyCollection
         );
 
         return $decryptedData;
     }
 
     /**
-     * @param $identity
      * @throws \Virgil\Sdk\Exceptions\VirgilException
      */
-    public function storePrivateKey($identity)
+    public function storePrivateKey(string $identity): void
     {
         $this->privateKeyStorage->store($this->keyPair->getPrivateKey(), $identity);
     }
 
     /**
-     * @param $identity
-     * @return \Virgil\Sdk\Storage\PrivateKeyEntry
      * @throws \Virgil\Sdk\Exceptions\VirgilException
      */
-    public function loadPrivateKey($identity)
+    public function loadPrivateKey(string $identity): PrivateKeyEntry
     {
         return $this->privateKeyStorage->load($identity);
     }
 
     /**
-     * @param $identity
      * @throws \Virgil\Sdk\Exceptions\VirgilException
      */
-    public function deletePrivateKey($identity)
+    public function deletePrivateKey(string $identity): void
     {
         $this->privateKeyStorage->delete($identity);
     }
 
     /**
-     * @param $identity
-     * @return Card[]
      * @throws \Virgil\Sdk\Exceptions\CardClientException
      * @throws \Virgil\Sdk\Exceptions\CardVerificationException
      */
-    public function getUserCardsByIdentity($identity)
+    public function getUserCardsByIdentity(string $identity): array
     {
         return $this->getCardManager()->searchCards($identity);
     }
 
     /**
-     * @param $id
-     * @return Card
      * @throws \Virgil\Sdk\Exceptions\CardClientException
      * @throws \Virgil\Sdk\Exceptions\CardVerificationException
      */
-    public function getUserCardById($id)
+    public function getUserCardById(int $id): Card
     {
         return $this->getCardManager()->getCard($id);
     }
 
     // PRIVATE FUNCTIONS:
 
-    /**
-     * @return VirgilCrypto
-     * @throws Exception
-     */
-    private function getCrypto()
+    private function getVirgilCrypto(): VirgilCrypto
     {
         return new VirgilCrypto();
     }
 
-    /**
-     * @return VirgilCardCrypto
-     */
-    private function getCardCrypto()
+    private function getCardVerifier(): VirgilCardVerifier
     {
-        return new VirgilCardCrypto();
+        return new VirgilCardVerifier(
+            $this->getVirgilCrypto(),
+            true,
+            true,
+            [],
+            $this->auth['serviceKey']
+        );
     }
 
     /**
-     * @return VirgilCardVerifier
-     */
-    private function getCardVerifier()
-    {
-        return new VirgilCardVerifier($this->getCardCrypto());
-    }
-
-    /**
-     * @return CardManager
      * @throws \Virgil\CryptoImpl\VirgilCryptoException
      */
-    private function getCardManager()
+    private function getCardManager(): CardManager
     {
-        return new CardManager($this->getCardCrypto(), $this->setUpJWTProvider(), $this->getCardVerifier());
+        return new CardManager(
+            $this->getVirgilCrypto(),
+            $this->setUpJWTProvider(),
+            $this->getCardVerifier(),
+            new CardClient(new HttpVirgilAgent(), $this->auth['serviceAddress'])
+        );
+    }
+
+    private function generateKeys(): VirgilKeyPair
+    {
+        return $this->getVirgilCrypto()->generateKeyPair();
     }
 
     /**
-     * @return \Virgil\CryptoImpl\VirgilKeyPair
      * @throws \Virgil\CryptoImpl\VirgilCryptoException
      */
-    private function generateKeys()
-    {
-        return $this->getCrypto()->generateKeys();
-    }
-
-    /**
-     * @return string
-     * @throws \Virgil\CryptoImpl\VirgilCryptoException
-     */
-    private function getGeneratedJWT()
+    private function getGeneratedJWT(): string
     {
         $privateKeyStr = $this->auth['apiKey'];
         $apiKeyData = base64_decode($privateKeyStr);
 
-        $privateKey = $this->getCrypto()->importPrivateKey($apiKeyData);
+        $privateKey = $this->getVirgilCrypto()->importPrivateKey($apiKeyData);
 
-        $accessTokenSigner = new VirgilAccessTokenSigner();
-
-        $jwtGenerator = new JwtGenerator($privateKey, $this->auth['apiKeyId'], $accessTokenSigner, $this->auth['appId'],
+        $jwtGenerator = new JwtGenerator($privateKey->getPrivateKey(), $this->auth['apiKeyId'],
+            $this->getVirgilCrypto(), $this->auth['appId'],
             $this->auth['ttl']);
 
         $token = $jwtGenerator->generateToken($this->identity);
@@ -245,14 +198,13 @@ class VirgilCryptoSample
     }
 
     /**
-     * @return CallbackJwtProvider
      * @throws \Virgil\CryptoImpl\VirgilCryptoException
      */
-    private function setUpJWTProvider()
+    private function setUpJWTProvider(): CallbackJwtProvider
     {
         $jwt = $this->getGeneratedJWT();
 
-        $authenticatedQueryToServerSide = function (TokenContext $context) use ($jwt) {
+        $authenticatedQueryToServerSide = function () use ($jwt) {
             return $jwt;
         };
 
@@ -260,16 +212,16 @@ class VirgilCryptoSample
     }
 
     /**
-     * @return Card
      * @throws \Virgil\CryptoImpl\VirgilCryptoException
-     * @throws \Virgil\Sdk\CardClientException
-     * @throws \Virgil\Sdk\CardVerificationException
+     * @throws \Virgil\Sdk\Exceptions\CardClientException
+     * @throws \Virgil\Sdk\Exceptions\CardVerificationException
      */
-    private function createCard()
+    private function createCard(): Card
     {
         return $this->getCardManager()->publishCard(
             CardParams::create(
                 [
+                    CardParams::Identity => $this->identity,
                     CardParams::PublicKey => $this->keyPair->getPublicKey(),
                     CardParams::PrivateKey => $this->keyPair->getPrivateKey(),
                 ]
